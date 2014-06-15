7e2499fa537b10279db1049c9f52a6b3b0f3183e
rexyeung <rex.627@gmail.com>
2014-06-15 15:08:44 -0700

    Update patient query to ajax

---
+++ html/patient_queries/patient_query1.php
@@ -0,0 +1,16 @@
+<?php
+  $mysqli = mysqli_connect("localhost", "root", "eece304Rocks!", "hospital");
+  // $mysqli = mysqli_connect("localhost", "root", "", "hospital");
+
+  $pid = $_POST["patient_select_name"];
+
+  $sql = "SELECT * FROM Assigned_Patient WHERE PID='".$pid."'";
+  $result = mysqli_query($mysqli, $sql);
+
+  while($row = mysqli_fetch_array($result)) {
+    echo $row['PName'];
+  }
+
+  /* close connection */
+  $mysqli->close();
+  ?>
