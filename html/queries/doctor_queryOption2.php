80a06a291ee55851a1efc2357ad11cf1dae87142
Khine <9khine.htwe@gmail.com>
2014-06-16 15:03:23 -0700

    Create doctor_queryOption2.php

---
+++ html/queries/doctor_queryOption2.php
@@ -0,0 +1,23 @@
+<?php
+echo '<form>';
+echo '<select name="Take" onchange="showQueryOptions(this.value)">';
+
+$con = mysqli_connect('localhost','root','eece304Rocks!','hospital');
+if (!$con) {
+  die('Could not connect: ' . mysqli_error($con));
+}
+
+$sql="SELECT DName FROM Disease";
+$result = mysqli_query($con,$sql);
+echo '<option value="">Select a treatment:</option>';
+while($row = mysqli_fetch_array($result)) {
+  echo '<option value="' . $row['Disease Name'] . '">' . $row['Disease Name'] . '</option>';
+}
+
+mysqli_close($con);
+
+echo "</select>
+</form>
+<br>
+      <div id='txtHint1'><b>Person info will be listed here.</b></div>"
+?>
