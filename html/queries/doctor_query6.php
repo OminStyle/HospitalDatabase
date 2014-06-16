8d9e7a455c1ef9b33d94d0e83a8df81da7fe727d
Khine <9khine.htwe@gmail.com>
2014-06-16 16:19:11 -0700

    Update doctor_query6.php

--- html/queries/doctor_query6.php
+++ html/queries/doctor_query6.php
@@ -1,20 +1,22 @@
 <?php
 $q = $_GET['q'];
 
-$con = mysqli_connect('localhost','root','eece304Rocks!','hospital');
+$con = mysqli_connect('localhost','eece304','eece304Rocks!','hospital');
 if (!$con) {
   die('Could not connect: ' . mysqli_error($con));
 }
 
 //mysqli_select_db($con,"hospital");
 //$sql="SELECT PID, PName FROM Assigned_Patient WHERE RoomNumber=120";
-$sql="SELECT PID, PName FROM Assigned_Patient WHERE RoomNumber='".$q."'";
+$sql="SELECT PID, PName 
+    FROM Assigned_Patient 
+    WHERE RoomNumber='$q'";
 $result = mysqli_query($con,$sql);
 
 echo "<table border='1'>
 <tr>
-  <th>Patient ID</th>
-  <th>Patient Name</th>
+  <th>PID</th>
+  <th>PName</th>
 </tr>";
 
 while($row = mysqli_fetch_array($result)) {
