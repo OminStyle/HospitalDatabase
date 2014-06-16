ef0b0702ed562fba8d760e0106a83d174fa5d7bb
OminStyle <name.cmh@gmail.com>
2014-06-15 17:55:10 -0700

    completed Query5

---
+++ html/queries/doctor_query5.php
@@ -0,0 +1,33 @@
+<?php
+// a disease have by all the patients over 70 years old
+$q = $_GET['q'];
+
+$con = mysqli_connect('localhost','eece304','eece304Rocks!','hospital');
+if (!$con) {
+  die('Could not connect: ' . mysqli_error($con));
+}
+
+$sql="SELECT DISTINCT di.DID, di.DName
+FROM Diagnose d, Disease di
+WHERE d.DID = di.DID
+AND d.PID NOT IN (SELECT    a.PID
+                FROM    Assigned_Patient a
+                WHERE   a.age < 70)";
+$result = mysqli_query($con,$sql);
+
+echo "<table border='1'>
+<tr>
+  <th>Disease ID</th>
+  <th>Disease Name</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result)) {
+  echo "<tr>";
+  echo "<td>" . $row['DID'] . "</td>";
+  echo "<td>" . $row['DName'] . "</td>";
+  echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+?>
