d4820715b45485ceae3361fc3df30d24513782a8
Khine <9khine.htwe@gmail.com>
2014-06-16 16:33:02 -0700

    Update doctor_queryOption2.php

--- html/queries/doctor_queryOption2.php
+++ html/queries/doctor_queryOption2.php
@@ -1,20 +1,20 @@
 <?php
+// medicines/drugs are associated with specific treatment
 $q = $_GET['q'];
-echo '<b>Make a selection:</b>';
-echo '<form>
-    <select name="Query2" onchange="showData(this.value, this.name)">';
-
+echo '<b>Make a selection:</b>
+<form>
+<select name="Query6" onchange="showData(this.value, this.name)">';
 
 $con = mysqli_connect('localhost','eece304','eece304Rocks!','hospital');
 if (!$con) {
   die('Could not connect: ' . mysqli_error($con));
 }
 
-$sql="SELECT DName FROM Disease";
+$sql="SELECT RoomNumber FROM Room";
 $result = mysqli_query($con,$sql);
-echo '<option value="">Select a Disease Name:</option>';
+echo '<option value="">Select a Room:</option>';
 while($row = mysqli_fetch_array($result)) {
-  echo '<option value="' . $row['DName'] . '">' . $row['DName'] . '</option>';
+  echo '<option value="' . $row['RoomNumber'] . '">' . $row['RoomNumber'] . '</option>';
 }
 
 mysqli_close($con);
@@ -22,6 +22,5 @@ mysqli_close($con);
 echo "</select>
 </form>
 <br>
-      <div id='queryData'><b>Disease Index and Disease will be listed here.</b></div>"
+      <div id='queryData'><b>Patients in selected Room will be listed here.</b></div>"
 ?>
-
