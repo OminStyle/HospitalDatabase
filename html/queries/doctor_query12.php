2ecef1f1b6543b3ea350ff525fe85ee7d10171e0
Khine <9khine.htwe@gmail.com>
2014-06-17 14:14:58 -0700

    Create doctor_query12.php

---
+++ html/queries/doctor_query12.php
@@ -0,0 +1,35 @@
+<?php
+// medicines/drugs are associated with specific treatment
+$q = $_GET['q'];
+
+$con = mysqli_connect('localhost','eece304','eece304Rocks!','hospital');
+if (!$con) {
+  die('Could not connect: ' . mysqli_error($con));
+}
+
+$sql="
+    SELECT	d.DName, Min(a.Age) as youngest
+	FROM		Disease d, Assigned_Patient  a. Diagnose di, HospitalStaff h
+	WHERE	d.DID = di.DID and a.PID = di.PID and di.HSID = h.HSID
+			and h.Experience >= ANY (SELECT 	AVG (h2.Experience)
+							FROM		HospitalStaff h2, Doctor do
+WHERE	h2.HSID = do.HSID)
+	GROUP BY	d.DName";
+$result = mysqli_query($con,$sql);
+
+echo "<table border='1'>
+<tr>
+  <th>DName</th>
+  <th>Min Age</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result)) {
+  echo "<tr>";
+  echo "<td>" . $row['DName'] . "</td>";
+  echo "<td>" . $row['youngest'] . "</td>";
+  echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+?>
