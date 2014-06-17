2ff32cc88db18c95fc36b2509c5ee425435bf930
Khine <9khine.htwe@gmail.com>
2014-06-17 14:24:04 -0700

    Update doctor_query12.php

--- html/queries/doctor_query12.php
+++ html/queries/doctor_query12.php
@@ -7,10 +7,11 @@ if (!$con) {
   die('Could not connect: ' . mysqli_error($con));
 }
 
-$sql="
-    SELECT	d.DName, Min(a.Age) as youngest FROM Disease d, Assigned_Patient  a. Diagnose di, HospitalStaff h WHERE	d.DID = di.DID and a.PID = di.PID and di.HSID = h.HSID
-and h.Experience >= ANY (SELECT 	AVG (h2.Experience)
-			FROM		HospitalStaff h2, Doctor do
+$sql="SELECT	d.DName, Min(a.Age) as youngest
+	FROM		Disease d, Assigned_Patient  a, Diagnose di, HospitalStaff h
+	WHERE	d.DID = di.DID and a.PID = di.PID and di.HSID = h.HSID
+			and h.Experience >= ANY (SELECT 	AVG (h2.Experience)
+							FROM		HospitalStaff h2, Doctor do
 WHERE	h2.HSID = do.HSID)
 	GROUP BY	d.DName";
 $result = mysqli_query($con,$sql);
