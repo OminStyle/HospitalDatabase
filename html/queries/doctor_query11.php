e1743e8d68e652c64ec7a0e993aa002eb53ab659
rexyeung <rex.627@gmail.com>
2014-06-17 16:05:52 -0700

    BUG: doctor query 11 syntax error

--- html/queries/doctor_query11.php
+++ html/queries/doctor_query11.php
@@ -3,14 +3,14 @@
 
 	$con = mysqli_connect('localhost','eece304','eece304Rocks!','hospital');
 
-	$sql = 'DELETE FROM Disease WHERE DName=' . $dname;
+	$sql = 'DELETE FROM Disease WHERE DName='. "\"" . $dname . "\"";
 	echo $sql;
 	
 	$result = mysqli_query($con,$sql);
 	mysqli_close($con);
 ?>
 
-<!-- <meta http-equiv="refresh" content="1;/HospitalDatabase/html/doctor.php"> -->
+<meta http-equiv="refresh" content="1;/HospitalDatabase/html/doctor.php">
 <script type="text/javascript">
-    // window.location.href = "/HospitalDatabase/html/doctor.php"
+    window.location.href = "/HospitalDatabase/html/doctor.php"
 </script>
