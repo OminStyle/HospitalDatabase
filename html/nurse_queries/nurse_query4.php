f237882cb6bf376e6b9d99afa6a6de51888ad6a9
rexyeung <rex.627@gmail.com>
2014-06-17 19:08:42 -0700

    Update nurse query 4

--- html/nurse_queries/nurse_query4.php
+++ html/nurse_queries/nurse_query4.php
@@ -3,17 +3,13 @@
 $column = $_POST['patient_update_info'];
 $value = $_POST['value'];
 $pid = $_POST['pid'];
-echo $column;
-echo $value;
-echo $pid;
-// echo 'values received';
 
-// $con = mysqli_connect('localhost','eece304','eece304Rocks!','hospital');
+$con = mysqli_connect('localhost','eece304','eece304Rocks!','hospital');
 
-// $sql="UPDATE Assigned_Patient SET " . $column . "=" . $value . "WHERE PID=" . $pid;
-// echo $sql;
+$sql="UPDATE Assigned_Patient SET " . $column . "=" . $value . " WHERE PID=" . $pid;
+echo $sql;
 
-// $result = mysqli_query($con,$sql);
+$result = mysqli_query($con,$sql);
 
-// mysqli_close($con);
+mysqli_close($con);
 ?>
