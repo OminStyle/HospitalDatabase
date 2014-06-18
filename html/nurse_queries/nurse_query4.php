d6941ad344cc5d29ef34c4297362deb0450d59a1
rexyeung <rex.627@gmail.com>
2014-06-17 19:58:50 -0700

    Updated nurse query 4

--- html/nurse_queries/nurse_query4.php
+++ html/nurse_queries/nurse_query4.php
@@ -21,7 +21,7 @@ $result = mysqli_query($con,$sql);
 mysqli_close($con);
 ?>
 
-<meta http-equiv="refresh" content="1;/HospitalDatabase/html/nurse.php">
+<!-- <meta http-equiv="refresh" content="1;/HospitalDatabase/html/nurse.php">
 <script type="text/javascript">
     window.location.href = "/HospitalDatabase/html/nurse.php"
-</script>
+</script> -->
