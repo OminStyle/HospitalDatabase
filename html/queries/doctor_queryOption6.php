14b62a7b3ef6a280226240022fb5148e72ab705c
Khine <9khine.htwe@gmail.com>
2014-06-17 17:40:25 -0700

    Update doctor_queryOption6.php

--- html/queries/doctor_queryOption6.php
+++ html/queries/doctor_queryOption6.php
@@ -2,8 +2,8 @@
 $q = $_GET['q'];
 
 
-echo '<b>Make a selection:</b>';
-echo '<form>
+echo '<b>Make a selection:</b>
+  <form>
   <select name="Query6" onchange="showData(this.value, this.name)">';
 
 $con = mysqli_connect('localhost','eece304','eece304Rocks!','hospital');
