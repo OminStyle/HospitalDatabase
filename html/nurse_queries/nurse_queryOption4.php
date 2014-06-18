8c7810e68bc0eadf3148e550a91f249fdf07bc90
rexyeung <rex.627@gmail.com>
2014-06-18 00:12:08 -0700

    Styling changes

--- html/nurse_queries/nurse_queryOption4.php
+++ html/nurse_queries/nurse_queryOption4.php
@@ -1,16 +1,16 @@
-<b>Select Attribute to Modify:</b>
+<b>Select One Attribute to Modify:</b>
 <form action="nurse_queries/nurse_query4.php" method="post">
 	<select name="patient_update_info" style="width: 100%">
-        <option value="RoomNumber">1. Room Number</option>
-        <option value="Address">2. Address </option>
-        <option value="CCN">3. Card Card Number </option>
-        <option value="PName">4. Patient Name</option>
-        <option value="Age">5. Age</option>
-        <option value="Weight">6. Weight</option>
-        <option value="Height">7. Height</option>
+        <option value="RoomNumber">Room Number</option>
+        <option value="Address">Address </option>
+        <option value="CCN">Card Card Number </option>
+        <option value="PName">Patient Name</option>
+        <option value="Age">Age</option>
+        <option value="Weight">Weight</option>
+        <option value="Height">Height</option>
     </select><br>
 
-	Value: <br><input type="text" name="value" style="width: 100%"><br>
-	PID : <br><input type="text" name="pid" style="width: 100%"><br>
+	New Value: <br><input type="text" name="value" style="width: 100%"><br>
+	Enter Patient(PID) : <br><input type="text" name="pid" style="width: 100%"><br>
 	<input type='submit'>
 </form>
