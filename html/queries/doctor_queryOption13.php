c81248b2113309ccaa936d973370198472fcd394
rexyeung <rex.627@gmail.com>
2014-06-18 16:59:39 -0700

    BUG Fix doctor query option 13

--- html/queries/doctor_queryOption13.php
+++ html/queries/doctor_queryOption13.php
@@ -1,4 +1,4 @@
-<form name="patientInfo" class="form-inline" action="nurse_queries/nurse_query2.php" onsubmit="return validateForm()" method="post">
+<form class="form-control" name="Query13" onchange="showData(this.value, this.name)">
 	PID: <br><input class="form-control" type="text" name="pid" style="width: 100%"><br>
 	HSID(Doctor): <br><input class="form-control" type="text" name="hsid" style="width: 100%"><br>
 	Disease Index: <br><input class="form-control" type="text" name="did" style="width: 100%"><br>
