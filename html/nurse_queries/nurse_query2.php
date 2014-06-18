629cb50ef1edd5bfca5c7ecee4d624db247aa2fa
rexyeung <rex.627@gmail.com>
2014-06-17 22:45:34 -0700

    Error test

--- html/nurse_queries/nurse_query2.php
+++ html/nurse_queries/nurse_query2.php
@@ -12,12 +12,12 @@
 	$con = mysqli_connect('localhost','eece304','eece304Rocks!','hospital');
 
 	// INSERT INTO Assigned_Patient VALUES (34, 3, NULL, 1220, “Taylor Fisher”, 45, NULL, 170);
-	$sql = "INSERT INTO Assigned_Patient VALUES (" .$pid. "," .$roomNumber. ",\"" .$address. "\"," .$ccn. ",\"" .$pname. "\"," .$age. "," .$weight. "," .$height. ")";
+	$mysqli->query("INSERT INTO Assigned_Patient VALUES (" .$pid. "," .$roomNumber. ",\"" .$address. "\"," .$ccn. ",\"" .$pname. "\"," .$age. "," .$weight. "," .$height. ")");
 	echo $sql;
 	
 	$result = mysqli_query($con,$sql);
 
-	echo $result->error;
+	echo $mysqli->error;
 	echo 'dude';
 	echo mysql_error($con);
 	echo mysql_errno($con);
