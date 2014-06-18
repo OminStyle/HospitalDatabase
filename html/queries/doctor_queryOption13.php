28fbd6d361fee6d5bb2576921e9d3d718e645ee8
Khine <9khine.htwe@gmail.com>
2014-06-18 12:51:07 -0700

    Create doctor_queryOption13.php

---
+++ html/queries/doctor_queryOption13.php
@@ -0,0 +1,13 @@
+// ?????? took from insert patient in nurse.php
+
+<form name="patientInfo" class="form-inline" action="nurse_queries/nurse_query2.php" onsubmit="return validateForm()" method="post">
+	PID: <br><input class="form-control" type="text" name="pid" style="width: 100%"><br>
+	Room Number: <br><input class="form-control" type="text" name="roomNumber" style="width: 100%"><br>
+	Address: <br><input class="form-control" type="text" name="address" style="width: 100%"><br>
+	Care Card Number: <br><input class="form-control" type="text" name="ccn" style="width: 100%"><br>
+	Patient Name: <br><input class="form-control" type="text" name="pname" style="width: 100%"><br>
+	Age: <br><input class="form-control" type="text" name="age" style="width: 100%"><br>
+	Weight: <br><input class="form-control" type="text" name="weight" style="width: 100%"><br>
+	Height: <br><input class="form-control" type="text" name="height" style="width: 100%"><br>
+	<input class="form-control" type='submit'>
+</form>
