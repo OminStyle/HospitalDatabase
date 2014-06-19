30b9beabaf67f47d3ae1ea831353a6237d24eff4
rexyeung <rex.627@gmail.com>
2014-06-18 17:02:10 -0700

    CSS Fix query option 13

--- html/queries/doctor_queryOption13.php
+++ html/queries/doctor_queryOption13.php
@@ -1,4 +1,4 @@
-<form class="form-control" name="Query13" onchange="showData(this.value, this.name)">
+<form name="Query13" onchange="showData(this.value, this.name)">
 	PID: <br><input class="form-control" type="text" name="pid" style="width: 100%"><br>
 	HSID(Doctor): <br><input class="form-control" type="text" name="hsid" style="width: 100%"><br>
 	Disease Index: <br><input class="form-control" type="text" name="did" style="width: 100%"><br>
