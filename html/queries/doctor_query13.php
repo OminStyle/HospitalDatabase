4558c91ccb4db020b3437d1f82e27e67e733807b
rexyeung <rex.627@gmail.com>
2014-06-18 17:15:20 -0700

    Update query 13

--- html/queries/doctor_query13.php
+++ html/queries/doctor_query13.php
@@ -2,9 +2,6 @@
 	$pid =  $_POST["pid"];
 	$hsid = $_POST["hsid"];
 	$did = $_POST["did"];
-	echo $pid;
-	echo $hsid;
-	echo $did;
 
 	$con = mysqli_connect('localhost','eece304','eece304Rocks!','hospital');
 
@@ -13,20 +10,14 @@
 	$result = mysqli_query($con,$sql);
 
 	if(mysqli_error($con) != NULL) {
-		echo $pid;
-		echo $hsid;
-		echo $did;
 		echo $sql . "<br>";
 		echo mysqli_error($con) . "<br>";
 		echo "Error Number: " . mysqli_errno($con);
 	}
 	else {
-		echo $pid . "<br>";
-		echo $hsid. "<br>";
-		echo $did. "<br>";
 		echo $sql . "<br>";
 		mysqli_close($con);
-		// echo "<meta http-equiv=\"refresh\" content=\"1;/HospitalDatabase/html/doctor.php\"><script type=\"text/javascript\">window.location.href = \"/HospitalDatabase/html/doctor.php\"</script>";
+		echo "<meta http-equiv=\"refresh\" content=\"1;/HospitalDatabase/html/doctor.php\"><script type=\"text/javascript\">window.location.href = \"/HospitalDatabase/html/doctor.php\"</script>";
 	}
 	mysqli_close($con);
 ?>
